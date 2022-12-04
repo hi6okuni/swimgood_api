37a4797e71b5f2a7579230972fe0c81372e180ae
hi6okuni <hi6okuni@gmail.com>
2022-12-04 21:04:25 +0900

    feat: add register, login, user, logout route

---
+++ backend/domain/users/users_dao.go
@@ -0,0 +1,66 @@
+package users
+
+import (
+	"log"
+
+	"github.com/hi6okuni/go-auth-api/backend/datasource/mysql/users_db"
+	"github.com/hi6okuni/go-auth-api/backend/utils/errors"
+)
+
+var (
+	queryInsertUser     = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?);"
+	queryGetUserByEmail = "SELECT id, first_name, last_name, email, password FROM users WHERE email = ?;"
+	queryGetUserByID    = "SELECT id, first_name, last_name, email FROM users WHERE id = ?;"
+)
+
+func (user *User) Save() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewBadRequestError("database error")
+	}
+	defer stmt.Close()
+
+	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.Password)
+	if saveErr != nil {
+		return errors.NewInternalServerError("database error")
+	}
+
+	userID, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError("database error")
+	}
+
+	user.ID = userID
+
+	log.Println("Saved user data")
+
+	return nil
+}
+
+func (user *User) GetByEmail() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryGetUserByEmail)
+	if err != nil {
+		return errors.NewInternalServerError("invalid email")
+	}
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.Email)
+	if getErr := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password); getErr != nil {
+		return errors.NewInternalServerError("database error")
+	}
+	return nil
+}
+
+func (user *User) GetByID() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryGetUserByID)
+	if err != nil {
+		return errors.NewInternalServerError("database error")
+	}
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.ID)
+	if getErr := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email); getErr != nil {
+		return errors.NewInternalServerError("database error")
+	}
+	return nil
+}
---
+++ backend/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hi6okuni/go-auth-api/backend/app"
+
+func main() {
+	app.StartApplication()
+}
