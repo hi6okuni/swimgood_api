8d6bbae98250dd15719ff1975e484262be79ac32
hi6okuni <hi6okuni@gmail.com>
2022-12-04 21:14:55 +0900

    fix: err def in sql.Open

--- backend/datasource/mysql/users_db/users_db.go
+++ backend/datasource/mysql/users_db/users_db.go
@@ -21,7 +21,7 @@ func init() {
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
 
 	var err error
-	Client, err := sql.Open("mysql", dataSourceName)
+	Client, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
