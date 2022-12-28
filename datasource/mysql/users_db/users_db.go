9b1ad53f30534298ed4fd49fc505cf91cf325181
hi6okuni <hi6okuni@gmail.com>
2022-12-29 01:11:20 +0900

    feat: set dataSourceName

--- datasource/mysql/users_db/users_db.go
+++ datasource/mysql/users_db/users_db.go
@@ -2,27 +2,28 @@ package users_db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
-	// "os"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
-	Client *sql.DB
-	// username = os.Getenv("MYSQL_USER")
-	// password = os.Getenv("MYSQL_PASSWORD")
-	// host     = os.Getenv("MYSQL_HOST")
-	// schema   = os.Getenv("MYSQL_DATABASE")
+	Client   *sql.DB
+	username = os.Getenv("MYSQL_USER")
+	password = os.Getenv("MYSQL_PASSWORD")
+	host     = os.Getenv("MYSQL_HOST")
+	schema   = os.Getenv("MYSQL_DATABASE")
 )
 
 func init() {
 	// username:password@tcp(host)/user_schema
-	// dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
 
 	var err error
-	Client, err = sql.Open("mysql", "docker:password@tcp(godockerDB)/godocker?charset=utf8")
+	Client, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
